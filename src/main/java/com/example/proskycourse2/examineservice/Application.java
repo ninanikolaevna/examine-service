106005ba7340f1887f5de89702ce7ab3cb6ca92e
ninanikolaevna <ninanikolaevna.t@gmail.com>
2025-02-09 23:55:56 +0300

    Initial

---
+++ src/main/java/com/example/proskycourse2/examineservice/Application.java
@@ -0,0 +1,13 @@
+package com.example.proskycourse2.examineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
